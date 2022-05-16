de1e40969bc119efd70bf48e48c778fa5a97c51f
No767 <73260931+No767@users.noreply.github.com>
2022-05-15 22:07:16 -0700

    dont use fontsource???

--- birthday-gift/src/main.tsx
+++ birthday-gift/src/main.tsx
@@ -5,7 +5,6 @@ import { Footer} from './components/footer'
 import { ChakraProvider } from '@chakra-ui/react'
 import './index.css'
 import theme from './components/themes/theme'
-import "@fontsource/Quicksand";
 import { DarkMode, GlobalStyle } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
